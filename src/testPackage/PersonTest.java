2e8f2c96ef3400516862f2520c043b93ce1797d0
MMadsen99 <math9082@edu.easj.dk>
2020-10-16 15:31:03 +0200

    Lesson 4 done

---
+++ src/testPackage/PersonTest.java
@@ -0,0 +1,20 @@
+package testPackage;
+
+import JavaForbeginner.Person;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTest extends Person {
+
+public void testPerson()  {
+    fail(" Not yet implemented");
+}
+
+public void testSetName(){
+    fail(" Not yet implemented");
+}
+public void testSetmaximumBooks(){
+    fail(" Not yet implemented");
+}
+
+}
