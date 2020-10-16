420a8bd26db69822981dd64d15081ceba6c6b151
MMadsen99 <math9082@edu.easj.dk>
2020-10-16 12:00:11 +0200

    Initial commit

---
+++ src/JavaForbeginner/Person.java
@@ -0,0 +1,23 @@
+package JavaForbeginner;
+
+
+import java.util.ArrayList;
+
+//classes starter altid med stort
+public class Person {
+    // koden skal skrives inde i klassen
+    // variabler og metoder skal altid starte med småt
+
+    private String name; // name of the person
+    private  int maximumBooks; // most books the person can check out
+
+
+    //constructor
+
+    public Person() {
+        name = "Peter";
+        maximumBooks = 3;
+    }
+
+}
+
